b0a8ca0604f0e14373a4d60b861e80bfaafc90e3
sitiaminaa <sitiaminatussadiah@mail.ugm.ac.id>
2025-10-27 08:21:59 +0700

    Update forwardkinematics.py

--- forwardkinematics.py
+++ forwardkinematics.py
@@ -23,8 +23,24 @@ def forward_kinematics_matrix(L1, L2, theta1_deg, theta2_deg):
 def inverse_kinematics(x_target, y_target, L1, L2):
     return theta1_deg, theta2_deg
 def plot_arm(x1, y1, x2, y2, target_x=None, target_y=None, L1=0, L2=0):
+    origin_x, origin_y = 0, 0
+    total_length = L1 + L2 + 2
+    plt.figure()
+    plt.plot([origin_x, x1, x2], [origin_y, y1, y2], 'o-', color='blue', linewidth=3, markersize=8, label='Lengan Robot')
+    plt.plot(origin_x, origin_y, 'o', color='red', markersize=10, label='Sendi 1 (Origin)')
+    plt.plot(x1, y1, 'o', color='red', markersize=10, label='Sendi 2 (Siku)')
+    plt.plot(x2, y2, 'o', color='green', markersize=10, label='End-Effector')
+    if target_x is not None and target_y is not None:
+        plt.plot(target_x, target_y, 'rx', markersize=15, label='Target (x,y)')
+        plt.axis('equal')
+    plt.xlim(-total_length, total_length)
+    plt.ylim(-total_length, total_length)
+    plt.grid()
+    plt.legend()
+    plt.title("Visualisasi 2-DoF Arm")
     plt.savefig("hasil.png")
     plt.show()
+
 if __name__ == "__main__":
     L1 = 88.0   
     L2 = 24.0   
@@ -43,4 +59,5 @@ if __name__ == "__main__":
     print(f"Koordinat Siku (Joint 2): (x={siku_x:.2f}, y={siku_y:.2f})")
     print(f"KOORDINAT TITIK AKHIR (End-Effector):(x={ujung_x:.2f}, y={ujung_y:.2f})")
     print("\nMenampilkan visualisasi...")
-    plot_arm(siku_x, siku_y, ujung_x, ujung_y, None, None, L1, L2)
+    plot_arm (siku_x, siku_y, ujung_x, ujung_y, None, None, L1, L2)
+    
