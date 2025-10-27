c8d154a479ae6c6cbbd2b371b439cbf8ebc9ed79
sitiaminaa <sitiaminatussadiah@mail.ugm.ac.id>
2025-10-27 07:52:40 +0700

    Create forwardkinematics.py

---
+++ forwardkinematics.py
@@ -0,0 +1,46 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def create_trans_matrix(theta_deg, length):
+    theta_rad = math.radians(theta_deg)
+    cos_t = np.cos(theta_rad)
+    sin_t = np.sin(theta_rad)
+    return np.array([
+        [cos_t, -sin_t, length * cos_t],
+        [sin_t,  cos_t, length * sin_t],
+        [    0,      0,             1]
+    ])
+def forward_kinematics_matrix(L1, L2, theta1_deg, theta2_deg):
+    T1 = create_trans_matrix(theta1_deg, L1)
+    T2 = create_trans_matrix(theta2_deg, L2)
+    T_final = np.dot(T1, T2)
+    x1 = T1[0, 2]
+    y1 = T1[1, 2]
+    x2 = T_final[0, 2]
+    y2 = T_final[1, 2]
+    return (x1, y1), (x2, y2)
+def inverse_kinematics(x_target, y_target, L1, L2):
+    return theta1_deg, theta2_deg
+def plot_arm(x1, y1, x2, y2, target_x=None, target_y=None, L1=0, L2=0):
+    plt.savefig("hasil.png")
+    plt.show()
+if __name__ == "__main__":
+    L1 = 88.0   
+    L2 = 24.0   
+    theta1_deg = 40.0  
+    theta2_deg = 30.0  
+    print(f"Menghitung FK Study Case")
+    print(f"Input:")
+    print(f"L1 (femur)={L1}")
+    print(f"L2 (tibia)={L2}")
+    print(f"Theta 1={theta1_deg} derajat")
+    print(f"Theta 2={theta2_deg} derajat")
+    (siku_x, siku_y), (ujung_x, ujung_y) = forward_kinematics_matrix(
+        L1, L2, theta1_deg, theta2_deg
+    )
+    print(f"\nOutput:")
+    print(f"Koordinat Siku (Joint 2): (x={siku_x:.2f}, y={siku_y:.2f})")
+    print(f"KOORDINAT TITIK AKHIR (End-Effector):(x={ujung_x:.2f}, y={ujung_y:.2f})")
+    print("\nMenampilkan visualisasi...")
+    plot_arm(siku_x, siku_y, ujung_x, ujung_y, None, None, L1, L2)
